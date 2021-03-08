faa3ac734fbc273a5a2f38227d830490ca76f8e3
Rohan Sharma <rs.sharma2014@gmail.com>
2021-03-08 15:27:33 -0500

    "fixing sketch syntax errors"

--- sketch.js
+++ sketch.js
@@ -1,14 +1,12 @@
-const { FILL, default: p5 } = require("./p5");
 
-Let mySound, amplitude;
+let song, amplitude;
+
 function preload() {
-    mySound = loadSound('milesdavis.mp3');
+    song = loadSound('milesdavis.mp3');
 }
-
 
 function setup() {
     createCanvas(displayWidth, displayHeight);
-    console.log("setup");
     amplitude = new p5.Amplitude();
   }
   
@@ -16,15 +14,15 @@ function setup() {
     background(2);
     let level = amplitude.getLevel();
     let diam = map(level, 0, 1, 10, 400);
-    Fill(255,0,0);
-    ellipse(150,150,diam,diam);
+    fill(255,0,0);
+    ellipse(displayWidth/2,displayHeight/2,diam,diam);
   }
   
 function mousePressed() {
-    if(mySound.isPlaying()) {
-        mySound.stop();
+    if(song.isPlaying()) {
+        song.stop()
     } else{
-        mySound.play();
+        song.play()
     }
 }
   
